8b80dbcbf9c549ca8f908080f38be1b9878c9ab8
Martin Smith <mcodesmith@gmail.com>
2021-09-21 19:00:07 -0700

    chore(rust): convert request-response snips to `Handle::call` method

--- implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/signer.rs
+++ implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/signer.rs
@@ -1,25 +1,39 @@
 use crate::{VaultRequestMessage, VaultResponseMessage, VaultSync, VaultSyncCoreError};
 use ockam_core::Result;
-use ockam_node::block_future;
+// Todo: Remove
+// handle call method blocks
+// use ockam_node::block_future;
 use ockam_vault_core::{Secret, Signature, Signer};
 
 impl Signer for VaultSync {
     fn sign(&mut self, secret_key: &Secret, data: &[u8]) -> Result<Signature> {
-        block_future(&self.ctx.runtime(), async move {
-            self.send_message(VaultRequestMessage::Sign {
-                secret_key: secret_key.clone(),
-                data: data.into(),
-            })
-            .await?;
+        let resp = self.handle.call(VaultRequestMessage::Sign {
+            secret_key: secret_key.clone(),
+            data: data.into(),
+        })?;
 
-            let resp = self.receive_message().await?;
+        if let VaultResponseMessage::Sign(s) = resp {
+            Ok(s)
+        } else {
+            Err(VaultSyncCoreError::InvalidResponseType.into())
+        }
 
-            if let VaultResponseMessage::Sign(s) = resp {
-                Ok(s)
-            } else {
-                Err(VaultSyncCoreError::InvalidResponseType.into())
-            }
-        })
+        // Todo: remove after tests pass
+        // block_future(&self.ctx.runtime(), async move {
+        //     self.send_message(VaultRequestMessage::Sign {
+        //         secret_key: secret_key.clone(),
+        //         data: data.into(),
+        //     })
+        //     .await?;
+        //
+        //     let resp = self.receive_message().await?;
+        //
+        //     if let VaultResponseMessage::Sign(s) = resp {
+        //         Ok(s)
+        //     } else {
+        //         Err(VaultSyncCoreError::InvalidResponseType.into())
+        //     }
+        // })
     }
 }
 
--- implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/verifier.rs
+++ implementations/rust/ockam/ockam_vault_sync_core/src/vault_sync/verifier.rs
@@ -1,6 +1,8 @@
 use crate::{VaultRequestMessage, VaultResponseMessage, VaultSync, VaultSyncCoreError};
 use ockam_core::Result;
-use ockam_node::block_future;
+// Todo: Remove
+// handle call method blocks
+// use ockam_node::block_future;
 use ockam_vault_core::{PublicKey, Signature, Verifier};
 
 impl Verifier for VaultSync {
@@ -10,21 +12,34 @@ impl Verifier for VaultSync {
         public_key: &PublicKey,
         data: &[u8],
     ) -> Result<bool> {
-        block_future(&self.ctx.runtime(), async move {
-            self.send_message(VaultRequestMessage::Verify {
-                signature: signature.clone(),
-                public_key: public_key.clone(),
-                data: data.into(),
-            })
-            .await?;
+        let resp = self.handle.call(VaultRequestMessage::Verify {
+            signature: signature.clone(),
+            public_key: public_key.clone(),
+            data: data.into(),
+        })?;
 
-            let resp = self.receive_message().await?;
+        if let VaultResponseMessage::Verify(s) = resp {
+            Ok(s)
+        } else {
+            Err(VaultSyncCoreError::InvalidResponseType.into())
+        }
 
-            if let VaultResponseMessage::Verify(s) = resp {
-                Ok(s)
-            } else {
-                Err(VaultSyncCoreError::InvalidResponseType.into())
-            }
-        })
+        // Todo: remove after tests pass
+        // block_future(&self.ctx.runtime(), async move {
+        //     self.send_message(VaultRequestMessage::Verify {
+        //         signature: signature.clone(),
+        //         public_key: public_key.clone(),
+        //         data: data.into(),
+        //     })
+        //     .await?;
+        //
+        //     let resp = self.receive_message().await?;
+        //
+        //     if let VaultResponseMessage::Verify(s) = resp {
+        //         Ok(s)
+        //     } else {
+        //         Err(VaultSyncCoreError::InvalidResponseType.into())
+        //     }
+        // })
     }
 }
